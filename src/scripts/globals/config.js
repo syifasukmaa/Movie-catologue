fb0acb22c320c82dc511dd40b6e0353effa5a0d8
Syifa Sukma Ramadhani <91242619+syifasukmaa@users.noreply.github.com>
2023-02-21 22:47:13 +0700

    Revert "Add dotenv webpack"

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: process.env.API_KEY_TMDB,
+  KEY: '194e5b507c000559175857bad00a8476',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
